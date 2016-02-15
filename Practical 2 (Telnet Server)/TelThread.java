3b589ea39ce8900d480f2db22aa8f4aa6d15689b
RentonMcIntyre <rentonmcintyre@yahoo.com>
2016-02-15 17:04:38 +0200

    Set up repository for prac 2
    Busy adding multiuser functionality currently and will have some screen
    control (colours and clearing at the very least) by the end of the day

---
+++ Practical 2 (Telnet Server)/TelThread.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ *
+ * @author Emilio Singh     u14006512
+ * @author Renton McIntyre  u14312710
+ *
+ */
+public class TelThread extends Thread {
+    private Socket mySocket;
+    public int score;
+    TelThread(Socket mySocket) {
+        this.mySocket=mySocket;
+        this.score=0;
+    }
+
+
+    public void run()
+    {
+       try{
+            PrintWriter out = new PrintWriter(mySocket.getOutputStream(),true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(mySocket.getInputStream()));
+            TelnetServer.setQ(TelnetServer.message(mySocket), mySocket);
+
+
+                String line;
+            while(in!= null && ((line = in.readLine().trim()) != null))
+            {
+
+              /*  if (line.equals("bye")) {
+                    TelnetServer.leave(mySocket);
+                    break;
+                  }else if(line.equals("Next Question"))
+                {
+
+                       TelnetServer.setQ(TelnetServer.message());
+                        //if (TelnetServer.checkAnswer(line,a)==true){TelnetServer.correctAns();} else {TelnetServer.wrongAns(a);}
+
+                }else if (line.equals("Score"))
+                {
+                    TelnetServer.giveScore(this);
+                }
+                else
+                {*/
+                    if (TelnetServer.checkAnswer(line)==true)   TelnetServer.correctAns(this, mySocket); 
+                    else TelnetServer.wrongAns(TelnetServer.getQ(mySocket), mySocket);
+
+                    TelnetServer.prompt(mySocket);
+                    int reply;
+                    line = in.readLine().trim();
+                    if(line.equals("1"))
+                    {
+                      TelnetServer.setQ(TelnetServer.message(mySocket), mySocket);
+                    }
+                    else
+                    {
+                      TelnetServer.giveScore(this, mySocket);
+                      TelnetServer.leave(mySocket);
+                      break;
+                    }
+               //}
+            }
+
+            try {
+                out.close();
+                in.close();
+            }catch(IOException e)
+            {
+                System.out.println(e.getMessage());
+            }
+        }
+        catch (IOException e)
+        {
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
---
+++ Practical 2 (Telnet Server)/TelnetServer.java
@@ -0,0 +1,308 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ *
+ * @author Emilio Singh     u14006512
+ * @author Renton McIntyre  u14312710
+ *
+ */
+public class TelnetServer {
+    private static ArrayList<Socket> users = new ArrayList<Socket>();
+    private static int Question=-1;
+    /**
+     * @param args the command line arguments
+     */
+    private static ArrayList<testNode> questions=new ArrayList<testNode>();
+    private static ArrayList<currentQuestion> currentQs = new ArrayList<currentQuestion>();
+
+    public static class testNode
+    {
+        private String question;
+        private ArrayList<String> answers=new ArrayList<>();
+    };
+    
+    public static class currentQuestion
+    {
+        private Socket user;
+        private int currQ;
+        
+        public currentQuestion(Socket u, int i)
+        {
+            user = u;
+            currQ = i;
+        }
+    }
+
+    public static void main(String[] args) throws IOException
+    {
+
+        int port=55555;
+        try
+
+        {
+            ServerSocket myServer=new ServerSocket(port);
+            constructTest();
+
+          while (true)
+            {
+                Socket client=myServer.accept();
+                new TelThread(client).start();
+                //list.add(0, client);
+                users.add(client);
+                currentQs.add(new currentQuestion(client, -1));
+                System.out.println("A new person has joined.");
+                announce("Welcome User to the Telnet Tester", client);
+                ///id++;
+             //   ins();
+
+            }
+        }
+         catch(IOException e)
+              {
+          System.out.println(e.getMessage());
+           }
+
+    }
+
+
+   synchronized static void announce(String msg, Socket client) throws IOException
+    {
+        PrintWriter p;
+        
+            p=new PrintWriter(client.getOutputStream(),true);
+            p.println(msg);
+    }
+
+    synchronized static void prompt(Socket client) throws IOException
+    {
+        PrintWriter p;
+             p = new PrintWriter(client.getOutputStream(), true);
+             p.println("Would you like another question, or would you like to stop here? (1 - Continue, 2 - Stop)");
+    }
+
+   synchronized static int message(Socket client) throws IOException
+   {
+        PrintWriter p;
+
+        Random number=new Random();
+        int n=number.nextInt(questions.size());
+
+
+            p=new PrintWriter(client.getOutputStream(),true);
+            p.println(" ");
+            p.println(questions.get(n).question);
+            char a='A';
+            for (int i=0;i<questions.get(n).answers.size();i++)
+            {
+                p.println(a+":"+questions.get(n).answers.get(i).substring(1));
+                a++;
+            }
+            p.println(" ");
+
+            p.println("Select an option");
+
+            return n;
+   }
+   synchronized static void leave(Socket client) throws IOException
+   {
+        PrintWriter p;
+            p=new PrintWriter(client.getOutputStream(),true);
+            p.println("Tester  has left");
+        users.remove(client);
+   }
+      synchronized static void giveScore(TelThread user, Socket client) throws IOException
+   {
+        PrintWriter p;
+            p=new PrintWriter(client.getOutputStream(),true);
+            p.println("Your score is: "+user.score);
+
+
+   }
+  /*    synchronized static void ins(Socket client) throws IOException
+      {
+        PrintWriter p;
+            p=new PrintWriter(tester.getOutputStream(),true);
+            p.println(" ");
+            p.println("Commands (To type in)");
+            p.println(" ");
+            p.println("Score- See your score");
+            p.println(" ");
+            p.println("Next Question - go to the next question of the test");
+            p.println(" ");
+            p.println("bye- leave the test");
+            p.println(" ");
+
+      }*/
+      
+   synchronized static boolean checkAnswer(String ans)
+   {
+
+       testNode tmp=new testNode();
+       tmp.question=questions.get(Question).question;
+       tmp.answers=questions.get(Question).answers;
+       int actualAns=-1,answer=-1;
+       switch (ans)
+       {
+           case "A":answer=0;break;
+           case "B":answer=1;break;
+           case "C":answer=2;break;
+           case "D":answer=3;break;
+           case "E":answer=4;
+       };
+       boolean answered=false;
+
+       for(int i=0;i<tmp.answers.size()&& answered==false;i++)
+       {
+           if(tmp.answers.get(i).startsWith("+") && answered==false)
+           {
+                              actualAns=i;
+           }
+           else if(tmp.answers.get(i).startsWith("+More")&& answered==false)
+               {
+                   actualAns=i;
+                   answered=true;
+               }
+           else if(tmp.answers.get(i).startsWith("+None")&& answered==false)
+               {
+                   actualAns=i;
+                   answered=true;
+               }
+
+
+       }
+
+
+       return (actualAns==answer);
+   }
+
+   synchronized static void correctAns(TelThread user, Socket client) throws IOException
+   {
+        PrintWriter p;
+
+            p=new PrintWriter(client.getOutputStream(),true);
+            p.println("Congratulations. You got it right!");
+            user.score++;
+            p.println(" ");
+   }
+
+   synchronized static void wrongAns(int a, Socket client) throws IOException
+   {
+        PrintWriter p;
+        String cAns="";
+        for (int i=0;i<questions.get(a).answers.size();i++)
+        {
+            if (questions.get(a).answers.get(i).startsWith("+"))
+                {
+
+                  for (int k=0;k<questions.get(a).answers.size();k++)
+                  {
+                      if (questions.get(a).answers.get(k).equals("+More than one of the above"))
+                      {
+                          cAns=questions.get(a).answers.get(k);
+                      }
+                      if (questions.get(a).answers.get(k).equals("+None of the above"))
+                      {
+                          cAns=questions.get(a).answers.get(k);
+                      }
+                  }
+
+               cAns=questions.get(a).answers.get(i);
+                }
+        }
+
+            p=new PrintWriter(client.getOutputStream(),true);
+            p.println("The correct answer is:"+ cAns.substring(1));
+            p.println(" ");
+            p.println("Enter input");
+
+   }
+
+    private static void constructTest() throws IOException
+   {
+
+
+
+       FileReader fr=new FileReader("quiz.txt");
+       BufferedReader textReader=new BufferedReader(fr);
+
+       String sline;
+       int q=0;
+       while ((sline=textReader.readLine())!=null)
+        {
+            textReader.mark(1000);
+            if (sline.charAt(0)=='?')
+            {
+                //textReader.mark(100);
+                testNode tmp=new testNode();
+                tmp.question=sline;
+                //textReader.skip(0);
+                while ((sline=textReader.readLine())!=null&&(sline.charAt(0)!='?'))
+                {
+
+                    tmp.answers.add(0,sline);
+                }
+                textReader.reset();
+                int onlyRight=0,onlyWrong=0;
+                for(int i=0;i<tmp.answers.size();i++)
+                {
+                   if (tmp.answers.get(i).startsWith("+")==true)
+                   {
+                       onlyRight++;
+                   } else if (tmp.answers.get(i).startsWith("-")==true)
+                    {
+                        onlyWrong++;
+                    }
+                }
+                if ((onlyRight>1) && (onlyWrong==0))
+                {
+                    tmp.answers.add("+More than one of the above");
+                }
+
+                if ((onlyRight==0) && (onlyWrong>1))
+                {
+                    tmp.answers.add("+None of the above");
+                }
+                questions.add(0,tmp);
+            }
+
+        }
+
+       }
+   synchronized static public void setQ(int a, Socket client)
+    {
+        for(int i = 0; i < currentQs.size(); ++i)
+        {
+            if(currentQs.get(i).user == client)
+            {
+                currentQs.get(i).currQ = a;
+            }
+        }
+    }
+   synchronized static public int getQ(Socket client)
+    {
+        for(int i = 0; i < currentQs.size(); ++i)
+        {
+           if(currentQs.get(i).user == client)
+            {
+                return currentQs.get(i).currQ;
+            }
+        }
+        return -1;
+    }
+}
